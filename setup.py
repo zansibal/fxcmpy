d302f8162dda22ef7516eb596085fbfb4a81abfa
zansibal <abrahamsson.richard@gmail.com>
2022-02-21 10:36:11 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    try:
+        with open('README.md', 'r') as f:
+            return f.read()
+    except:
+        return ''
+
+
+setup(
+    name = 'fxcmpy',
+    packages = ['fxcmpy'], # this must be the same as the name above
+    version = '1.2.10',
+    description = 'A Python Wrapper Class for the RESTful API as provided by FXCM Forex Capital Markets Ltd.',
+    long_description = readme(),
+    author = 'The Python Quants GmbH',
+    author_email = 'admin@tpq.io',
+    license='BSD',
+    url = 'https://github.com/fxcm/fxcmpy', 
+    download_url = 'https://github.com/fxcm/fxcmpy', 
+    keywords = 'FXCM API Python Wrapper Finance Algo Trading',
+    install_requires=['pandas', 'socketIO_client', 'configparser', 'requests'], 
+    python_requires='>=3.4',
+    include_package_data = True,
+    package_data={
+        '': ['*.txt']
+    },
+    classifiers = ['Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6'],
+)
